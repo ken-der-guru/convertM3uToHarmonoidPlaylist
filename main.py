f3d52e1acc49d9ade575931a2fd026e4f9556e85
ken-der-guru <>
2023-04-22 22:57:50 +0200

    added comments & handling of extended m3u files

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os
 import pathlib
 import time
-import eyed3
 import logging
 import sys
 import json
 
+import eyed3
+
+
 logger = logging.getLogger(__name__)
 # Set logging level
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)# remove newline at end of line)
 
 formatter = logging.Formatter("%(message)s ")
 
@@ -16,10 +17,12 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 
-
+# Initialize m3u_path as Path() -> while loop works
 m3u_path = pathlib.Path()
+# wait till a valid Path to .m3u playlist was supplied
 while m3u_path.is_file() is False:
     m3u_path = pathlib.Path(input('enter valid URI to m3u Playlist:\n'))
+
 # Format of playlists in the Harmonoid Playlist.JSON file 
 #playlistDictHarmonoid = [{'name': None,
 #                          'id': None,
@@ -39,17 +42,24 @@ while m3u_path.is_file() is False:
 #                              }],
 #                          }]
 
+# m3u specification:
+# https://en.wikipedia.org/wiki/M3U
+
 with open(m3u_path) as f:
     tracks = []
+    playlistTitle = ' '
     for i, line in enumerate(f):
-        if '#EXTINF' in line:
+        line = line.replace('\n', '')  # remove newline at end of line
+        if '#PLAYLIST' in line:
+            playlistTitle = line.replace('#PLAYLIST', '')  # set title of playlist
+
+        if '#' in line:  # '#' indicates that the line is a comment or additional unnecessary info -> skip line
             continue
-        if '/' in line:
-            path = line.replace('\n', '')
-            path = pathlib.Path(path)
-            audiofile = eyed3.load(path)
-            logger.debug(audiofile.tag.track_num[0])
-            # TODO print(audiofile.tag.track_num[0])
+        if '/' in line:  # '/' indicates that the line is a Path to a song
+            path = pathlib.Path(line)
+
+            audiofile = eyed3.load(path.absolute())  # ope the file with eyed3 to read its metadata
+
             try:
                 artists = [audiofile.tag.artist]
             except AttributeError:
@@ -69,27 +79,31 @@ with open(m3u_path) as f:
             })
 
 logger.debug(tracks)
-logger.info(f"Title of Playlist: '{m3u_path.name[0:m3u_path.name.rfind('.m3u')]}'")
-playlistDictHarmonoid = {'name': m3u_path.name[0:m3u_path.name.rfind('.m3u')],
+# If there was no title in .m3u file set it to the name of the .m3u file
+if playlistTitle == '':
+    playlistTitle = m3u_path.name[0:m3u_path.name.rfind('.m3u')]
+
+logger.info(f"Title of Playlist: '{playlistTitle}'")
+playlistDictHarmonoid = {'name': playlistTitle,
                          'id': 3,
                          'tracks': tracks, }
 # print(playlistDictHarmonoid)
 
 #print(json.dumps(playlistDictHarmonoid, indent=4))
 
-harmonoidPlaylist_path = os.path.join(pathlib.Path.home(), '.Harmonoid', 'Playlists.JSON')
-answer = input(f"Is this the path to your Harmonoid Playlists.JSON file?\n'{harmonoidPlaylist_path}'\n[yes/no]?\n")
-while answer != 'yes' and answer != 'no':
+harmonoidPlaylist_path = pathlib.Path.joinpath(pathlib.Path.home(), '.Harmonoid', 'Playlists.JSON')
+
+answer = str.lower(input(f"Is this the path to your Harmonoid Playlists.JSON file?\n'{harmonoidPlaylist_path}'\n["
+                         f"yes/no]?\n"))
+while answer != 'yes' and answer != 'y' and answer != 'no' and answer != 'n':
     logger.debug(f'answer: {answer}')
-    answer = input('[yes/no]?\n')
-if answer == 'no':
+    answer = str.lower(input('[yes/no]?\n'))
+if answer == 'no' or answer == 'n':
     harmonoidPlaylist_path = pathlib.Path(input('enter valid Path to your Harmonoid Playlists.JSON file:\n'))
     while harmonoidPlaylist_path.is_file() is False:
         harmonoidPlaylist_path = pathlib.Path(input('enter valid Path to your Harmonoid Playlists.JSON file:\n'))
 
 
-
-f = None
 with open(harmonoidPlaylist_path) as f:
     data = json.load(f)
     r = -2
@@ -106,6 +120,7 @@ with open(harmonoidPlaylist_path) as f:
     s.append(playlistDictHarmonoid)
     playlistNewHarmonoid = {"playlists": s}
     #print(json.dumps(playlistNewHarmonoid, indent=4))
+
 with open(harmonoidPlaylist_path, 'w') as f:
     f.write(json.dumps(playlistNewHarmonoid, indent=4))
     print('Added playlist to Harmonoid')
